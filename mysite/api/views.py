57c0b8d767d918724401e19d8ada14e69a1aad8e
mo.ibrahh <108186067+moibrahh@users.noreply.github.com>
2024-09-20 16:46:02 +0000

    new

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from rest_framework import generics
+from .models import BLogPost
+
+
+
 # Create your views here.
 
 
-class BlogPostListCreate(generics.ListCreateAPIViews)
+class BlogPostListCreate(generics.ListCreateAPIViews):
+    queryset = BlogPost.object.all()
